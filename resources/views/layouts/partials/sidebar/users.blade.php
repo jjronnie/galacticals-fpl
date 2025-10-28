a7d7b50ecace0306cd2d1c9175e13331f8dee831
Jjuuko Ronald <ronaldjjuuko7@gmail.com>
2025-10-28 16:57:27 +0300

    Update users.blade.php

--- resources/views/layouts/partials/sidebar/users.blade.php
+++ resources/views/layouts/partials/sidebar/users.blade.php
@@ -5,7 +5,7 @@
            class="flex flex-col items-center justify-center w-full p-2 text-white transition-colors duration-200 
                   {{ request()->routeIs('dashboard') ? 'text-yellow-400' : 'hover:text-yellow-200' }}">
             <i data-lucide="layout-dashboard" class="w-5 h-5"></i>
-            <span class="text-xs font-medium mt-1">Stats</span>
+            <span class="text-xs font-medium mt-1">Dashboard</span>
         </a>
         
         <a href="#" 
@@ -18,8 +18,8 @@
         <a href="{{ route('profile.edit') }}" 
            class="flex flex-col items-center justify-center w-full p-2 text-white transition-colors duration-200 
                   {{ request()->routeIs('profile.') ? 'text-yellow-400' : 'hover:text-yellow-200' }}">
-            <i data-lucide="wrench" class="w-5 h-5"></i>
-            <span class="text-xs font-medium mt-1">Settings</span>
+            <i data-lucide="user" class="w-5 h-5"></i>
+            <span class="text-xs font-medium mt-1">My Account</span>
         </a>
         
         </div>
