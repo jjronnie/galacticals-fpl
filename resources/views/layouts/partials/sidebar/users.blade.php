83b4ee5046eba64451b6f75d6ec7abebd7b59cc5
Jjuuko Ronald <ronaldjjuuko7@gmail.com>
2025-10-29 11:47:52 +0300

    Update users.blade.php

--- resources/views/layouts/partials/sidebar/users.blade.php
+++ resources/views/layouts/partials/sidebar/users.blade.php
@@ -1,12 +1,12 @@
 <nav class="fixed bottom-0 left-0 right-0 bg-primary border-t border-gray-700 z-50">
     <div class="flex justify-around items-center h-16 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
-          <a href="{{ route('home') }}" 
+          {{-- <a href="{{ route('home') }}" 
            class="flex flex-col items-center justify-center w-full p-2 text-white transition-colors duration-200 
                   {{ request()->routeIs('home') ? 'text-yellow-400' : 'hover:text-yellow-200' }}">
             <i data-lucide="house" class="w-5 h-5"></i>
             <span class="text-xs font-medium mt-1">Home</span>
-        </a>
+        </a> --}}
         
         <a href="{{ route('dashboard') }}" 
            class="flex flex-col items-center justify-center w-full p-2 text-white transition-colors duration-200 
