26f3128d6b3a2a9381bee64940cf52478492c8b8
Jjuuko Ronald <ronaldjjuuko7@gmail.com>
2025-11-10 09:50:56 +0300

    Update league-stats.blade.php

--- resources/views/league-stats.blade.php
+++ resources/views/league-stats.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="robots" content="index, follow" />
-    <title>   {{ $league->name ?? }} - FPL Galaxy Stats </title>
+    <title>   {{ $league->name ?? 'STATS' }} - FPL Galaxy Stats </title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <!-- Favicon -->
