1750bbca815c1a3f90410498207b7a759952f3e6
Jjuuko Ronald <ronaldjjuuko7@gmail.com>
2025-12-04 19:51:09 +0300

    Update league-stats.blade.php

--- resources/views/league-stats.blade.php
+++ resources/views/league-stats.blade.php
@@ -25,7 +25,7 @@
             <div class="p-6 text-center">
 
                 <a href="#current" class="btn">
-                    View Current GW
+                    GO to Current GW
                 </a>
 
             </div>
