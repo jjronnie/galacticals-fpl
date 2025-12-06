889bffe5ca414e5da610ac3d6d3a8671b79b4dac
Jjuuko Ronald <ronaldjjuuko7@gmail.com>
2025-12-06 13:38:49 +0300

    Update league-update-started.blade.php

--- resources/views/emails/league-update-started.blade.php
+++ resources/views/emails/league-update-started.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>League Update mail</title>
 </head>
-<body>
+
+<body style="background-color: teal; color: white;">
 
     <p>Hello,</p>
-<p>Your league update command has just started running.</p>
-<p>Regards.</p>
-    
+    <p>Your league update command has just started running.</p>
+    <p>Regards.</p>
+
 </body>
+
 </html>
-
-
