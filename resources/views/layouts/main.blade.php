93c7f6b87877ce8cd78daea5d36bb3326136f2e9
Jjuuko Ronald <ronaldjjuuko7@gmail.com>
2025-12-09 09:40:26 +0300

    MICROSOFT CLARITY

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,6 +30,16 @@
   gtag('config', 'G-36Z6T6DYQ4');
     </script>
 
+    {{-- CLARITY --}}
+
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "uip1r477ae");
+</script>
+
 
 
 
