71ca091e2e5d9ccf05bdf7f3e64768cc06c5db89
Jjuuko Ronald <ronaldjjuuko7@gmail.com>
2025-12-11 11:37:12 +0300

    Update welcome-user.blade.php

--- resources/views/emails/welcome-user.blade.php
+++ resources/views/emails/welcome-user.blade.php
@@ -18,8 +18,19 @@ How to Find my League ID
 
 ### Watch the Instruction Video
 
-
-[![Video](https://img.youtube.com/vi/{{ substr($youtubeLink, strrpos($youtubeLink, '/') + 1) }}/hqdefault.jpg)]({{ str_replace('/embed/', '/watch?v=', $youtubeLink) }})
+<table role="presentation" width="100%" cellpadding="0" cellspacing="0" border="0">
+    <tr>
+        <td align="center">
+            <a href="{{ str_replace('/embed/', '/watch?v=', $youtubeLink) }}" target="_blank" style="text-decoration:none;">
+                <img 
+                    src="https://img.youtube.com/vi/{{ substr($youtubeLink, strrpos($youtubeLink, '/') + 1) }}/hqdefault.jpg"
+                    alt="Watch Video"
+                    style="display:block; width:100%; max-width:600px; height:auto; margin:0 auto; border-radius:8px;"
+                >
+            </a>
+        </td>
+    </tr>
+</table>
 
 If you have any questions, feel free to reply to this email.
 
